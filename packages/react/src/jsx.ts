559d4c57e0ec167d1a6bb315361dcc566b54c2c3
Zhiyuan Guo <zhiyuan.guo@shopee.com>
2024-09-12 15:33:38 +0800

    feat: v2

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,5 +1,13 @@
-const jsx = () => {
-	return 'hello world';
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols';
+
+const jsx = (type: string, props: any, key: string | number) => {
+	return {
+		'&&typeof': REACT_ELEMENT_TYPE,
+		type,
+		props,
+		key,
+		ref: null
+	};
 };
 
 export const jsxDEV = jsx;
